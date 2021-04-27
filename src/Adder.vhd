88836d5f005ffdf9213e582ad571c1941b8b4920
Juan Perez <juan.perez@example.com>
2021-04-27 17:22:35 -0300

    Entities of Example1.vhd and Adder.vhd

--- src/Adder.vhd
+++ src/Adder.vhd
@@ -1,1 +1,11 @@
-fffff
+library ieee;
+use ieee.std_logic_1164.all;
+entity adder is
+port(x1, x2 : in std_logic_vector(3 downto 0);
+result: out std_logic_vector(3 downto 0);
+carry : out std_logic_vector);
+end example1;
+architecture bev of adder
+begin
+--TODO
+end bev;
--- src/Ejemplo1.vhd
+++ src/Ejemplo1.vhd
@@ -1,1 +1,10 @@
-iee
+library ieee;
+use ieee.std_logic_1164.all;
+entity example1 is
+port(x1, x2 : in std_logic_vector(3 downto 0);
+displays: out std_logic_vector(13 downto 0) );
+end example1;
+architecture bev of example1
+begin
+--TODO
+end bev;

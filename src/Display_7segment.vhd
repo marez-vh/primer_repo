ea8ec949b35c1a5e7e7e6cb904d67022b3d2cebd
Juan Perez <juan.perez@example.com>
2021-04-27 17:24:25 -0300

    Entity of Display_7segment

--- src/Display_7segment.vhd
+++ src/Display_7segment.vhd
@@ -1,1 +1,11 @@
-aaaaaa
+library ieee;
+use ieee.std_logic_1164.all;
+entity display_7segment is
+port( x1 : in std_logic_vector(4 downto 0);
+-- two 7-segment display will be used.
+displays: out std_logic_vector(13 downto 0) );
+end display_7segment;
+architecture bev of display_7segment
+begin
+--todo
+end bev;
